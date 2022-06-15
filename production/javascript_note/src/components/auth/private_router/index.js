2ae0a84dbfcdf23f934dc007eaa16a95d729f2e0
Gabriel <gabrielferreirasantos723@gmail.com>
2022-06-15 17:46:14 -0300

    feito correção das rotas para funcionar corretamente

--- production/javascript_note/src/components/auth/private_router/index.js
+++ production/javascript_note/src/components/auth/private_router/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route, Navigate} from 'react-router-dom';
 
-const privateRoute = ({component: Component, ...rest}) => (
+const privateRoute = ({ component: Component, ...rest}) => (
     <Route {...rest} render={props => (
         localStorage.getItem('user')
         ?   <Component {...props} />
