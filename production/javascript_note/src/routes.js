dd55d643452cc43ff8208c367fd374d86640c448
Gabriel <gabrielferreirasantos723@gmail.com>
2022-06-15 17:47:49 -0300

    feito correção das rotas para funcionar corretamente

--- production/javascript_note/src/routes.js
+++ production/javascript_note/src/routes.js
@@ -11,15 +11,15 @@ import LoginScreen from './screens/auth/login';
 import NotesScreen from './screens/notes/index';
 import UsersEditScreen from './screens/users/edit';
 
-import privateRoute from './components/auth/private_router';
+import PrivateRouter from './components/auth/private_router';
 
 const App = () => {
     let routes = useRoutes([
         { path: '/', element: <HomeScreen /> },
         { path: '/register', element: <RegisterScreen /> },
         { path: '/login', element: <LoginScreen /> },
-        { privateRoute: '/notes', element: <NotesScreen /> },
-        { privateRoute: '/users/edit', element: <UsersEditScreen /> },
+        { path: '/notes', element: <NotesScreen /> },
+        { path: '/users/edit', element: <UsersEditScreen /> },
 
     ]);
     return routes;
